4b7a41aa7cead693869a1024060a1867eca971c6
David Beniamine <david.beniamine@imag.fr>
2015-04-14 21:23:09 -0300

    initial commit

---
+++ IS/npbparams.h
@@ -0,0 +1,14 @@
+#define CLASS 'W'
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   */
+   
+#define COMPILETIME "14 Apr 2015"
+#define NPBVERSION "3.3.1"
+#define CC "cc"
+#define CFLAGS "-O2 -g -fopenmp"
+#define CLINK "$(CC)"
+#define CLINKFLAGS "-O2 -fopenmp"
+#define C_LIB "-lnuma"
+#define C_INC "(none)"
